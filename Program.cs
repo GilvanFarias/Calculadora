fe31fee38facafaa93f6b363419e75d718ddb4c9
Gilvan Farias <63085957+GilvanFarias@users.noreply.github.com>
2021-06-17 11:19:00 -0300

    calculadora 1.0

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+using System;
+using System.Collections.Generic;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int res = 0;
+            string operador;
+
+            Console.Write("Digite um valor inteiro: ");
+            int valor1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um operador: ");
+            operador = Console.ReadLine();
+
+            
+            if(operador == "*" ||operador == "/" ||operador == "+" ||operador == "-"){
+
+                //multiplicar
+                if(operador == "*"){
+                    Console.Write("Digite um valor inteiro: ");
+                    int valor2 = int.Parse(Console.ReadLine());
+                    res = valor1 * valor2;
+                    Console.Write("Resultado de: "+valor1 + " * " +valor2+ " = " + res);
+                //dividir
+                }if(operador == "/"){
+
+                    Console.Write("Digite um valor inteiro: ");
+                    int valor2 = int.Parse(Console.ReadLine());
+                    res = valor1 / valor2;
+                    Console.Write("Resultado de: "+valor1 + " / " +valor2+ " = " + res);
+                //somar
+                }if(operador == "+"){
+
+                    Console.Write("Digite um valor inteiro: ");
+                    int valor2 = int.Parse(Console.ReadLine());
+                    res = valor1 + valor2;
+                    Console.Write("Resultado de: "+valor1 + " + " +valor2+ " = " + res);
+
+                //diminuir
+                }if(operador == "-"){
+
+                    Console.Write("Digite um valor inteiro: ");
+                    int valor2 = int.Parse(Console.ReadLine());
+                    res = valor1 - valor2;
+                    Console.Write("Resultado de: "+valor1 + " - " +valor2+ " = " + res);
+                }
+
+            }else{
+
+                    Console.WriteLine("Digite um operador Válido");
+            }
+
+                
+            
+        }
+    }
+}
